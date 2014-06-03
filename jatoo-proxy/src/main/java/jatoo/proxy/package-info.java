e54d249cea5b0677ec1c63e56216dd4d14af7b17
Cristian Sulea <cristian.sulea.79@gmail.com>
2014-06-03 23:40:15 +0300

    update package-info.java

--- jatoo-proxy/src/main/java/jatoo/proxy/package-info.java
+++ jatoo-proxy/src/main/java/jatoo/proxy/package-info.java
@@ -1,4 +1,6 @@
 /**
+ * A handy, tiny java component created to ease the work with Internet proxies.
+ * 
  * @author Cristian Sulea ( http://cristian.sulea.net )
  * @version 1.0, June 3, 2014
  */
