24c3e184e428255dd9add8bfdd3f02d628863b9e
Cristian Sulea <cristian.sulea.79@gmail.com>
2014-06-18 15:19:41 +0300

    update description

--- jatoo-proxy/src/main/java/jatoo/proxy/package-info.java
+++ jatoo-proxy/src/main/java/jatoo/proxy/package-info.java
@@ -1,8 +1,7 @@
 /**
- * A handy, tiny java component created to ease the work with Internet proxies.
+ * A Java Open Source library created to ease the work with Internet proxies.
  * 
  * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
- * @version 1.0, June 3, 2014
  */
 package jatoo.proxy;
 
